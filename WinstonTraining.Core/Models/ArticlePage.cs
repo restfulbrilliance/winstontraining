c844218f872090aab8ec7e5a3352524911142c5c
Robert Strube <rstrube@gmail.com>
2018-03-27 10:28:29 -0700

    Lesson 2: Added Title property with custom getter / setter.  Added controller and view for ArticlePage.

--- WinstonTraining.Core/Models/ArticlePage.cs
+++ WinstonTraining.Core/Models/ArticlePage.cs
@@ -8,12 +8,36 @@ namespace WinstonTraining.Core.Models
     [ContentType(DisplayName = "Article Page", GUID = "5b1030e9-1072-4554-976b-3200f7dab43e", Description = "Basic Article Page")]
     public class ArticlePage : SitePageData
     {
-        [CultureSpecific]
+        [Display(
+            Name = "Title",
+            Description = "Article title, falls back to Name if not defined.",
+            GroupName = SystemTabNames.Content,
+            Order = 2000)]
+        public virtual string Title
+        {
+            get
+            {
+                var tmpTitle = this.GetPropertyValue(p => p.Title);
+                return string.IsNullOrEmpty(tmpTitle) ? Name : tmpTitle;
+            }
+
+            set
+            {
+                this.SetPropertyValue(p => p.Title, value);
+            }
+        }
+
         [Display(
             Name = "Main body",
             Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
             GroupName = SystemTabNames.Content,
-            Order = 2000)]
+            Order = 2100)]
         public virtual XhtmlString MainBody { get; set; }
+
+        public string Test()
+        {
+            var articlePage = new ArticlePage();
+            return articlePage.Title;
+        }
     }
 }
---
+++ WinstonTraining.Web/Controllers/ArticlePageController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using WinstonTraining.Core.Models;
+
+namespace WinstonTraining.Web.Controllers
+{
+    public class ArticlePageController : PageController<ArticlePage>
+    {
+        public ActionResult Index(ArticlePage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
