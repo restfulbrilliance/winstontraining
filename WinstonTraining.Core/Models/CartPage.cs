3087c05efb163ade2f1145bb7449095d7a4b5872
Robert Strube <rstrube@gmail.com>
2018-08-27 09:24:08 -0500

    More work an asynchronous cart operations.

---
+++ WinstonTraining.Core/Models/CartPage.cs
@@ -0,0 +1,8 @@
+﻿using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace WinstonTraining.Core.Models
+{
+    [ContentType(DisplayName = "Cart Page", GUID = "23517d05-12e7-4ce0-90e3-ce86c4b85c8d", Description = "")]
+    public class CartPage : ArticlePage { }
+}
---
+++ WinstonTraining.Web/Controllers/CartPageController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using WinstonTraining.Core.Models;
+
+namespace WinstonTraining.Web.Controllers
+{
+    public class CartPageController : PageController<CartPage>
+    {
+        public ActionResult Index(CartPage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
