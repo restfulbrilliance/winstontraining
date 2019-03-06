d8f112d523bedfac8d148a43503466b67231b40f
Robert Strube <rstrube@gmail.com>
2019-03-06 12:09:28 -0600

    Added in some JS work.

---
+++ WinstonTraining.Web/Scripts/winstonTraining.js
@@ -0,0 +1,27 @@
+﻿var _winstonTraining = window.winstonTraining || {};
+
+_winstonTraining = (function ($) {
+
+    function publicMethod1() {
+        alert("public method 1");
+    }
+
+    function publicMethod2() {
+        alert("public method 2");
+    }
+
+    function privateMethod() {
+        alert("private method");
+    }
+
+    //DOM ready for the JavaScript class
+    $(function () {
+        $('.js-TeaserHeader').css({ color: 'red' });
+    });
+
+    return {
+        method1: publicMethod1,
+        method2: publicMethod2
+    };
+
+})(jQuery);
